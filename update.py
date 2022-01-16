38ddd967c26fad1a980d52060ab2ee4e5d8f92d9
daczarne <daniel.czarnievicz@gmail.com>
2022-01-16 12:41:12 +0100

    adds update.py which will run data/fetch_data.py and analysis/build_plot.py

---
+++ update.py
@@ -0,0 +1,13 @@
+print("Fetching repo data...")
+print("-----------------------")
+
+from data import fetch_data
+
+print("All repos data fetched!")
+print("-----------------------")
+print("Building plot...")
+
+from analysis import build_plot
+
+print("-----------------------")
+print("All done!")
